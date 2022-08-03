ca563e64caeaf8c80de93eb77a0e66e7d2756b00
Andrés Gutiérrez <psirusteam@Andress-MacBook-Pro.local>
2022-08-03 13:41:49 -0400

    directos

--- DirectosDOM.r
+++ DirectosDOM.r
@@ -1,3 +1,62 @@
-library(srvyr)
+rm(list = ls())
 
-dom <- readRDS("encuestaDOM20N1.rds")
+library(survey)
+library(srvyr)
+library(dplyr)
+
+encuesta <- readRDS("encuestaDOM20N1.rds")
+names(encuesta)
+
+encuesta$estrato <- encuesta$'_estrato'
+table(encuesta$estrato)
+encuesta$upm <- encuesta$'_upm'
+table(encuesta$upm)
+encuesta$fep <- encuesta$'_fep'
+summary(encuesta$fep)
+sum(encuesta$fep)
+
+encuesta$pobre <- as.numeric(ifelse(encuesta$pobreza != 3, 1, 0))
+encuesta$one <- as.factor(1)
+
+table(encuesta$pobre)
+
+diseno <- encuesta %>% 
+  as_survey_design(strata = estrato,
+                   ids = upm,
+                   weights = fep,
+                   nest = TRUE)
+
+options(survey.lonely.psu = "adjust")
+
+summary(diseno)
+
+consulta1 <- diseno %>% 
+  summarise(
+    totaling = survey_total(ingcorte)
+  )
+
+
+consulta1 <- diseno %>% 
+  group_by(id_provincia, areageo2) %>% 
+  summarise(
+    mediaing = survey_mean(ingcorte,
+                           vartype = c("se", "cv", "ci"))
+  )
+
+
+consulta1 %>% as.data.frame()
+
+consulta2 <- diseno %>% 
+  group_by(id_provincia, areageo2) %>% 
+  summarise(
+    proppob = survey_mean(pobre,
+                            vartype = c("se", "cv", "ci"), )
+    )
+ 
+
+
+consulta2 %>% as.data.frame()
+
+    
+    
+    
